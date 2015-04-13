960a386e1a7fa5292d47b8975adc1b9491bd9a75
isos81 <ismetbahadir@gmail.com>
2015-04-13 14:48:13 +0300

    src

---
+++ src/test/java/org/spring/SpringvBoardAppApplicationTests.java
@@ -0,0 +1,19 @@
+package org.spring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.spring.vboard.VBoardApplication;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = VBoardApplication.class)
+@WebAppConfiguration
+public class SpringvBoardAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
